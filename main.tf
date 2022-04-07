86c048f5d1d6f6d28ca0ced02d69bdc358e4bed9
AlexVoysest <91563276+AlexVoysest@users.noreply.github.com>
2022-04-07 19:20:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ terraform {
 }resource “aws_instance” “app_server” {
   ami           = “ami-0c02fb55956c7d316”
   instance_type = “t2.micro”  tags = {
-    Name = “AlexV2AppServerInstance”
+    'Name = “AlexV2AppServerInstance”'
   }
 }
