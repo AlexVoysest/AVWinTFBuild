5bf8c41c74984df6ddcf6d2af77b5520e3a65f41
AlexVoysest <avoysest@paloaltonetworks.com>
2022-07-07 13:05:03 -0400

    Change Instance Name

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "app_server" {
   ami = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   tags = {
-    Name = "AlexV2AppServerInstance"
+    Name = "AlexVAppServerInstance"
   }
 }
